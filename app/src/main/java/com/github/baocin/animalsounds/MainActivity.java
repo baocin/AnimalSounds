b1a585389f58a7dac8176c525fd109d855eaa018
Michael Pedersen <steele.pedersen@gmail.com>
2016-01-05 06:13:43 -0500

    Can now choose an answer! With visual feedback on wrong and right answers!

--- app/src/main/java/com/github/baocin/animalsounds/MainActivity.java
+++ app/src/main/java/com/github/baocin/animalsounds/MainActivity.java
@@ -1,20 +1,25 @@
 package com.github.baocin.animalsounds;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import im.delight.android.baselib.Screen;
+import im.delight.android.baselib.UI;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     static boolean lockedOrientation = false;
+    public int numCorrect = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
@@ -35,13 +39,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+        if (id == R.id.refresh_button) {
+            UI.restartActivity(this);
             return true;
         }
         if (id == R.id.lock_orientation){
@@ -50,19 +55,17 @@ public class MainActivity extends AppCompatActivity {
                 Screen.unlockOrientation(this);
             }else {
                 Screen.lockOrientation(this);
+                Log.d(TAG, "Orientation: " + getResources().getConfiguration().orientation);
                 switch (getResources().getConfiguration().orientation) {
-                    case Screen.Orientation.LANDSCAPE:
-                    case Screen.Orientation.REVERSE_LANDSCAPE:
+                    case Configuration.ORIENTATION_LANDSCAPE:
                         Toast.makeText(this.getApplicationContext(), "Locked in Landscape", Toast.LENGTH_SHORT).show();
                         break;
-                    case Screen.Orientation.PORTRAIT:
-                    case Screen.Orientation.REVERSE_PORTRAIT:
+                    case Configuration.ORIENTATION_PORTRAIT:
                         Toast.makeText(this.getApplicationContext(), "Locked in Portrait", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
             lockedOrientation = !lockedOrientation;
-
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/github/baocin/animalsounds/MainActivityFragment.java
+++ app/src/main/java/com/github/baocin/animalsounds/MainActivityFragment.java
@@ -1,5 +1,7 @@
 package com.github.baocin.animalsounds;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,27 +14,39 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "MainActivityFragment";
-    public HashMap<String, Integer> animals = new HashMap<>();
-    public int correctAnimal;
+    private static final int LEFT_ANIMAL_ID = 0;
+    private static final int RIGHT_ANIMAL_ID = 1;
+    public HashMap<String, Integer> animalImages = new HashMap<>();
+    public HashMap<String, Integer> animalAudio = new HashMap<>();
+    private static MediaPlayer mp = new MediaPlayer();
+
+    public int correctAnimalID = 0;
+    public ArrayList<String> pickedAnimals = new ArrayList<>();
 
     public MainActivityFragment() {
-        animals.put("cat", R.drawable.cat_photo);
-        animals.put("cow", R.drawable.cow_photo);
-        animals.put("donkey", R.drawable.donkey_photo);
-        animals.put("frog", R.drawable.frog_photo);
-        animals.put("horse", R.drawable.horse_photo);
-        animals.put("pig", R.drawable.pig_photo);
-        animals.put("sheep", R.drawable.sheep_photo);
+        animalImages.put("cat", R.drawable.cat_photo);
+        animalImages.put("cow", R.drawable.cow_photo);
+        animalImages.put("donkey", R.drawable.donkey_photo);
+        animalImages.put("frog", R.drawable.frog_photo);
+        animalImages.put("horse", R.drawable.horse_photo);
+        animalImages.put("pig", R.drawable.pig_photo);
+        animalImages.put("sheep", R.drawable.sheep_photo);
+
+        animalAudio.put("cat", R.raw.cat);
+        animalAudio.put("cow", R.raw.cow);
+        animalAudio.put("donkey", R.raw.donkey);
+        animalAudio.put("frog", R.raw.frog);
+        animalAudio.put("horse", R.raw.horse);
+        animalAudio.put("pig", R.raw.pig);
+        animalAudio.put("sheep", R.raw.sheep);
     }
 
     @Override
@@ -40,40 +54,108 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
+        setupAnimals(v);
+
+        return v;
+    }
+    public void resetAnimals(View v){
+        //Stop audio (if playing)
+        mp.stop();
+
+        //Clear background colors
         ImageView leftAnimal = (ImageView) v.findViewById(R.id.leftImage);
         ImageView rightAnimal = (ImageView) v.findViewById(R.id.rightImage);
-        leftAnimal.setOnClickListener(this);
 
-        ArrayList<String> pickedAnimals = pickNItems(animals.keySet(), 2);
-        leftAnimal.setImageResource(animals.get(pickedAnimals.get(0)));
-        rightAnimal.setImageResource(animals.get(pickedAnimals.get(1)));
+        leftAnimal.setBackgroundColor(Color.TRANSPARENT);
+        rightAnimal.setBackgroundColor(Color.TRANSPARENT);
 
         TextView leftText = (TextView) v.findViewById(R.id.leftText);
         TextView rightText = (TextView) v.findViewById(R.id.rightText);
-        leftText.setText(pickedAnimals.get(0));
-        rightText.setText(pickedAnimals.get(1));
 
         leftText.setVisibility(View.INVISIBLE);
         rightText.setVisibility(View.INVISIBLE);
 
-        TextView questionText = (TextView) v.findViewById(R.id.questionText);
-        questionText.setText("Choose between the " + pickedAnimals.get(0) + " and the " + pickedAnimals.get(1));
+        leftText.setBackgroundColor(Color.TRANSPARENT);
+        rightText.setBackgroundColor(Color.TRANSPARENT);
 
-        return v;
+        leftText.setAllCaps(false);
+        rightText.setAllCaps(false);
+    }
+
+    public void setupAnimals(View v){
+        resetAnimals(v);
+
+        ImageView leftAnimal = (ImageView) v.findViewById(R.id.leftImage);
+        ImageView rightAnimal = (ImageView) v.findViewById(R.id.rightImage);
+        leftAnimal.setOnClickListener(this);
+        rightAnimal.setOnClickListener(this);
+
+        pickedAnimals = pickNItems(animalImages.keySet(), 2);
+        leftAnimal.setImageResource(animalImages.get(pickedAnimals.get(LEFT_ANIMAL_ID)));
+        rightAnimal.setImageResource(animalImages.get(pickedAnimals.get(RIGHT_ANIMAL_ID)));
+
+        TextView leftText = (TextView) v.findViewById(R.id.leftText);
+        TextView rightText = (TextView) v.findViewById(R.id.rightText);
+        leftText.setText(pickedAnimals.get(LEFT_ANIMAL_ID));
+        rightText.setText(pickedAnimals.get(RIGHT_ANIMAL_ID));
+
+
+
+        TextView questionText = (TextView) v.findViewById(R.id.questionText);
+//        questionText.setText("Choose between the " + pickedAnimals.get(LEFT_ANIMAL_ID) + " and the " + pickedAnimals.get(RIGHT_ANIMAL_ID));
+        questionText.setText("Choose the animal that makes this sound!");
+
+        //pick the correct answer
+        correctAnimalID = randomRange(LEFT_ANIMAL_ID, RIGHT_ANIMAL_ID); //pick either 0 and 1
+
+        //Play the correct animal sound
+        mp = MediaPlayer.create(this.getActivity(), animalAudio.get(pickedAnimals.get(correctAnimalID)));
+        mp.start();
     }
 
     public void onClick(View v) {
-        if (v.getId() == R.id.leftImage){
-            Toast.makeText(v.getContext(), "L", Toast.LENGTH_LONG).show();
+        TextView leftText = (TextView) getActivity().findViewById(R.id.leftText);
+        TextView rightText = (TextView) getActivity().findViewById(R.id.rightText);
+        ImageView leftAnimal = (ImageView) getActivity().findViewById(R.id.leftImage);
+        ImageView rightAnimal = (ImageView) getActivity().findViewById(R.id.rightImage);
+
+        //Bold the correct animal name
+        if (correctAnimalID == LEFT_ANIMAL_ID){
+            leftText.setAllCaps(true);
+            leftAnimal.setBackgroundColor(getResources().getColor(R.color.green));
+            leftText.setBackgroundColor(getResources().getColor(R.color.green));
+            rightAnimal.setBackgroundColor(getResources().getColor(R.color.red));
+            rightText.setBackgroundColor(getResources().getColor(R.color.red));
+        }else {
+            rightText.setAllCaps(true);
+            leftAnimal.setBackgroundColor(getResources().getColor(R.color.red));
+            leftText.setBackgroundColor(getResources().getColor(R.color.red));
+            rightAnimal.setBackgroundColor(getResources().getColor(R.color.green));
+            rightText.setBackgroundColor(getResources().getColor(R.color.green));
         }
-            Toast.makeText(v.getContext(),
-                    "T" + v.getId(),
-                    Toast.LENGTH_LONG).show();
 
-    }
-
-    public void playAnimalSound(){
-
+        if (v.getId() == R.id.leftImage){
+            if (correctAnimalID == LEFT_ANIMAL_ID){
+                //CORRECT
+                Toast.makeText(v.getContext(), pickedAnimals.get(LEFT_ANIMAL_ID) + " is CORRECT!", Toast.LENGTH_SHORT).show();
+                setupAnimals(getView());
+            }else {
+                Toast.makeText(v.getContext(), pickedAnimals.get(LEFT_ANIMAL_ID) + " is incorrect, TRY AGAIN?", Toast.LENGTH_SHORT).show();
+            }
+            leftText.setVisibility(View.VISIBLE);
+            rightText.setVisibility(View.VISIBLE);
+        }
+        if (v.getId() == R.id.rightImage){
+            if (correctAnimalID == RIGHT_ANIMAL_ID){
+                //CORRECT
+                Toast.makeText(v.getContext(), pickedAnimals.get(RIGHT_ANIMAL_ID) + " is CORRECT!", Toast.LENGTH_SHORT).show();
+                setupAnimals(getView());
+            }else {
+                Toast.makeText(v.getContext(), pickedAnimals.get(RIGHT_ANIMAL_ID) + " is incorrect, TRY AGAIN?", Toast.LENGTH_SHORT).show();
+            }
+            leftText.setVisibility(View.VISIBLE);
+            rightText.setVisibility(View.VISIBLE);
+        }
     }
 
     public int randomRange(int min, int max) {
